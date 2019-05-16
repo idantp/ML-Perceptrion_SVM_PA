dc75813b1b02af2c0b063c0a07aa985cae15bb9a
idant <idant@LAPTOP-1CBL0P6J>
2019-05-16 15:40:34 +0300

    Errors rate decrease

--- ex2.py
+++ ex2.py
@@ -7,13 +7,33 @@ from scipy.stats import zscore
 
 
 # TODO: normalization z-score ?
+# TODO: check that the normalization is correct.
+
+def zero_score_norm(x_arr):
+    for i in range(0, len(x_arr)):
+        x_arr[i] = zscore(x_arr[i])
+    return x_arr
+
+
+def test(test_x, test_y, w, algo):
+    results = []
+    num = 0
+    m = len(test_x)
+    for i in range(0, m):
+        y_hat = np.argmax(np.dot(w, test_x[i]))
+        if test_y[i] != y_hat:
+            results.append(y_hat)
+            num += 1
+    print(algo, "err: ", (float(num) / m), " good: ", 1 - (float(num) / m))
+    return results
+
 
 def normalization(x_arr):
     transposed_x_arr = np.transpose(x_arr)
     for row in transposed_x_arr:
         max_in_col = max(row)
         min_in_col = min(row)
-        if (max_in_col - min_in_col != 0):
+        if max_in_col - min_in_col != 0:
             row = (row - min_in_col) / (max_in_col - min_in_col)
         else:
             continue
@@ -21,7 +41,7 @@ def normalization(x_arr):
 
 
 def svm(x_arr, y_arr):
-    iterations = 10
+    iterations = 200
     eta = 0.01
     var_lambda = 0.3
     weights_arr = np.zeros((3, 8))
@@ -42,7 +62,7 @@ def svm(x_arr, y_arr):
                         weights_arr[y_hat, :] = (1 - (var_lambda * eta)) * weights_arr[y_hat,
                                                                            :] + np.multiply(eta, x)
                     else:
-                        weights_arr[i, :] = (1 - (var_lambda * eta)) * weights_arr[i,
+                        weights_arr[matrix_line_no, :] = (1 - (var_lambda * eta)) * weights_arr[matrix_line_no,
                                                                        :] + np.multiply(eta, x)
     return weights_arr
 
@@ -62,7 +82,8 @@ def perceptron_algorithm(x_arr, y_arr):
             if y != y_hat:
                 weights_arr[y, :] = weights_arr[y, :] + np.multiply(eta, x)
                 weights_arr[y_hat, :] = weights_arr[y_hat, :] - np.multiply(eta, x)
-    print(weights_arr)
+        if i > iterations/2:
+            eta/=i
     return weights_arr
 
 
@@ -71,11 +92,11 @@ def loss_function_pa(weights_arr, y, y_hat, x):
 
 
 def pa_algorithm(x_arr, y_arr):
-    iterations = 10
+    iterations = 15
     weights_arr = np.zeros((3, 8))
     for i in range(iterations):
         arr_zip = list(zip(x_arr, y_arr))
-        np.random.shuffle(arr_zip)
+        # np.random.shuffle(arr_zip)
         x_arr, y_arr = zip(*arr_zip)
         for x, y in zip(x_arr, y_arr):
             # predict.
@@ -122,11 +143,13 @@ def read_from_files(x_arr, y_arr):
             line = path2.readline()
     return counter
 
+
 def array_splitter(start_index,end_index,array):
     temp_arr = []
     for row in range(start_index,end_index):
         temp_arr.append(array[row])
     return temp_arr
+
 
 def main():
     x_arr = []
@@ -136,11 +159,20 @@ def main():
     testing_sets_index = training_sets_amount + 1
     y_arr = list(map(int, y_arr))
     x_arr = normalization(x_arr)
+    # x_arr = zero_score_norm(x_arr)
     arr_zipped = list(zip(x_arr, y_arr))
     np.random.shuffle(arr_zipped)
-    x_training_sets = array_splitter(0,training_sets_amount,x_arr)
-    x_testing_sets = array_splitter(testing_sets_index,(sets_amount+1),x_arr)
-    perceptron_algorithm(x_training_sets, y_arr)
+    shuffled_x_arr,shuffled_y_arr = zip(*arr_zipped)
+    x_training_sets = array_splitter(0,training_sets_amount,shuffled_x_arr)
+    x_testing_sets = array_splitter(testing_sets_index,(sets_amount),shuffled_x_arr)
+    y_training_sets = array_splitter(0,training_sets_amount,shuffled_y_arr)
+    y_testing_sets = array_splitter(testing_sets_index,(sets_amount),shuffled_y_arr)
+    w = perceptron_algorithm(x_training_sets, y_training_sets)
+    w2 = pa_algorithm(x_training_sets, y_training_sets)
+    w3 = svm(x_training_sets, y_training_sets)
+    test(x_testing_sets,y_testing_sets,w,"PERCEPTRON")
+    test(x_testing_sets,y_testing_sets,w2,"PA")
+    # test(x_testing_sets,y_testing_sets,w3,"SVM")
 
 
 if __name__ == "__main__":
