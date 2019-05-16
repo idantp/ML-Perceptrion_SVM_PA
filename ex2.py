8e4c74b496e66b07cee4bdd0ef046a557a45b7da
idant <idant@LAPTOP-1CBL0P6J>
2019-05-16 19:23:30 +0300

    SVM correction

--- ex2.py
+++ ex2.py
@@ -30,20 +30,21 @@ def test(test_x, test_y, w, algo):
 
 def normalization(x_arr):
     transposed_x_arr = np.transpose(x_arr)
+    temp_arr = []
     for row in transposed_x_arr:
         max_in_col = max(row)
         min_in_col = min(row)
         if max_in_col - min_in_col != 0:
-            row = (row - min_in_col) / (max_in_col - min_in_col)
+            temp_arr.append((row - min_in_col) / (max_in_col - min_in_col))
         else:
             continue
-    return np.transpose(transposed_x_arr)
+    return np.transpose(temp_arr)
 
 
 def svm(x_arr, y_arr):
-    iterations = 200
-    eta = 0.01
-    var_lambda = 0.3
+    iterations = 10
+    eta = 0.05
+    var_lambda = 0.1
     weights_arr = np.zeros((3, 8))
     for i in range(iterations):
         arr_zip = list(zip(x_arr, y_arr))
@@ -56,14 +57,18 @@ def svm(x_arr, y_arr):
             if y != y_hat:
                 for matrix_line_no in range(weights_arr.shape[0]):
                     if matrix_line_no == y:
-                        weights_arr[y, :] = (1 - (var_lambda * eta)) * weights_arr[y,
-                                                                       :] + np.multiply(eta, x)
+                        weights_arr[y, :] = np.multiply((1 - (var_lambda * eta)),
+                                                        weights_arr[y, :]) + np.multiply(eta, x)
                     elif matrix_line_no == y_hat:
-                        weights_arr[y_hat, :] = (1 - (var_lambda * eta)) * weights_arr[y_hat,
-                                                                           :] + np.multiply(eta, x)
+                        weights_arr[y_hat, :] = np.multiply((1 - (var_lambda * eta)),
+                                                            weights_arr[y_hat, :]) - np.multiply(
+                            eta, x)
                     else:
-                        weights_arr[matrix_line_no, :] = (1 - (var_lambda * eta)) * weights_arr[matrix_line_no,
-                                                                       :] + np.multiply(eta, x)
+                        weights_arr[matrix_line_no, :] = np.multiply((1 - (var_lambda * eta)),
+                                                                     weights_arr[matrix_line_no,
+                                                                     :]) + np.multiply(eta, x)
+        if i > iterations / 2:
+            eta /= i
     return weights_arr
 
 
@@ -82,8 +87,8 @@ def perceptron_algorithm(x_arr, y_arr):
             if y != y_hat:
                 weights_arr[y, :] = weights_arr[y, :] + np.multiply(eta, x)
                 weights_arr[y_hat, :] = weights_arr[y_hat, :] - np.multiply(eta, x)
-        if i > iterations/2:
-            eta/=i
+        if i > iterations / 2:
+            eta /= i
     return weights_arr
 
 
@@ -92,11 +97,11 @@ def loss_function_pa(weights_arr, y, y_hat, x):
 
 
 def pa_algorithm(x_arr, y_arr):
-    iterations = 15
+    iterations = 10
     weights_arr = np.zeros((3, 8))
     for i in range(iterations):
         arr_zip = list(zip(x_arr, y_arr))
-        # np.random.shuffle(arr_zip)
+        np.random.shuffle(arr_zip)
         x_arr, y_arr = zip(*arr_zip)
         for x, y in zip(x_arr, y_arr):
             # predict.
@@ -104,7 +109,7 @@ def pa_algorithm(x_arr, y_arr):
             # update
             if y != y_hat:
                 loss = loss_function_pa(weights_arr, y, y_hat, x)
-                divide_by = ((np.power(np.linalg.norm(x, ord=2), 2)) * 2)
+                divide_by = ((np.power(np.linalg.norm(x), 2)) * 2)
                 if divide_by != 0:
                     loss /= divide_by
                     weights_arr[y, :] = weights_arr[y, :] + np.multiply(loss, x)
@@ -144,9 +149,9 @@ def read_from_files(x_arr, y_arr):
     return counter
 
 
-def array_splitter(start_index,end_index,array):
+def array_splitter(start_index, end_index, array):
     temp_arr = []
-    for row in range(start_index,end_index):
+    for row in range(start_index, end_index):
         temp_arr.append(array[row])
     return temp_arr
 
@@ -156,23 +161,26 @@ def main():
     y_arr = []
     sets_amount = read_from_files(x_arr, y_arr)
     training_sets_amount = int((sets_amount * 5) / 6)
-    testing_sets_index = training_sets_amount + 1
+    testing_sets_index = training_sets_amount
     y_arr = list(map(int, y_arr))
     x_arr = normalization(x_arr)
     # x_arr = zero_score_norm(x_arr)
     arr_zipped = list(zip(x_arr, y_arr))
+    # TODO
     np.random.shuffle(arr_zipped)
-    shuffled_x_arr,shuffled_y_arr = zip(*arr_zipped)
-    x_training_sets = array_splitter(0,training_sets_amount,shuffled_x_arr)
-    x_testing_sets = array_splitter(testing_sets_index,(sets_amount),shuffled_x_arr)
-    y_training_sets = array_splitter(0,training_sets_amount,shuffled_y_arr)
-    y_testing_sets = array_splitter(testing_sets_index,(sets_amount),shuffled_y_arr)
+    shuffled_x_arr, shuffled_y_arr = zip(*arr_zipped)
+    x_training_sets = array_splitter(0, training_sets_amount, shuffled_x_arr)
+    x_testing_sets = array_splitter(testing_sets_index, sets_amount, shuffled_x_arr)
+    x_training_sets = np.array(x_training_sets)
+    x_testing_sets = np.array(x_testing_sets)
+    y_training_sets = array_splitter(0, training_sets_amount, shuffled_y_arr)
+    y_testing_sets = array_splitter(testing_sets_index, sets_amount, shuffled_y_arr)
     w = perceptron_algorithm(x_training_sets, y_training_sets)
     w2 = pa_algorithm(x_training_sets, y_training_sets)
     w3 = svm(x_training_sets, y_training_sets)
-    test(x_testing_sets,y_testing_sets,w,"PERCEPTRON")
-    test(x_testing_sets,y_testing_sets,w2,"PA")
-    # test(x_testing_sets,y_testing_sets,w3,"SVM")
+    test(x_testing_sets, y_testing_sets, w, "PERCEPTRON")
+    test(x_testing_sets, y_testing_sets, w2, "PA")
+    test(x_testing_sets, y_testing_sets, w3, "SVM")
 
 
 if __name__ == "__main__":
